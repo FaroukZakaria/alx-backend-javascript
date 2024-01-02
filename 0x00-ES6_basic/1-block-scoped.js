ce48c7e3f458fa3adca56ca50e09468bec61d8db
Farouk Zakaria <123017280+FaroukZakaria@users.noreply.github.com>
2024-01-02 23:46:57 +0200

    Modifying variables in a block-scope

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,18 @@
+export default function taskBlock(trueOrFalse) {
+    var task = false;
+    var task2 = true;
+
+    {
+        task = true;
+    }
+    {
+        task2 = false;
+    }
+    
+    if (trueOrFalse) {
+      var task = true;
+      var task2 = false;
+    }
+  
+    return [task, task2];
+  }
