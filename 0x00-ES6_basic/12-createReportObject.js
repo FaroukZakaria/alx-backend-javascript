b92fdf952511c1ca44cfa5ec1ef414438ac85297
FaroukZakaria <gameswithoutlose97@gmail.com>
2024-01-03 23:46:08 +0200

    didn't think there would be a comma dangle error

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,6 +2,6 @@ export default function createReportObject(employeesList) {
   const getNumberOfDepartments = () => Object.keys(employeesList).length;
   return {
     allEmployees: employeesList,
-    getNumberOfDepartments
+    getNumberOfDepartments,
   };
 }
