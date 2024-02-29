fabc50016ed4a444574856efe53a80ff6e96248b
FaroukZakaria <GamesWithoutLose97@gmail.com>
2024-02-29 16:08:49 +0000

    fixing

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -4,10 +4,10 @@ const sendPaymentRequestToApi = require('./3-payment.js');
 const Utils = require('./utils.js');
 
 describe('sendPaymentRequestToApi', () => {
-	it('should call Utils.calculateNumber with correct arguments', () => {
-		const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
-		sendPaymentRequestToApi(100, 20);
-		expect(calculateNumberSpy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
-		calculateNumberSpy.restore();
-	});
+  it('should call Utils.calculateNumber with correct arguments', () => {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+    expect(calculateNumberSpy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    calculateNumberSpy.restore();
+  });
 });
