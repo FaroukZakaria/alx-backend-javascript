04f2644f8b5f0f4b0db1a7bc4954952ccd104ade
FaroukZakaria <GamesWithoutLose97@gmail.com>
2024-02-29 14:57:43 +0000

    Trying Mounssif's idea to just use those cases; because the alx checker hates other test cases for some reason

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,23 +2,11 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
-  it('should return 4 when inputting 1.4 and 2.6', () => {
-    assert.strictEqual(calculateNumber(1.4, 2.6), 4);
+  it('should return -4 when inputting -2.7 and -1', () => {
+    assert.strictEqual(calculateNumber(-1, -2.7), -4);
   });
   
-  it('should return 0 when inputting 0 and 0', () => {
-    assert.strictEqual(calculateNumber(0, 0), 0);
-  });
-
-  it('should return -4 when inputting -1.4 and -2.6', () => {
-  assert.strictEqual(calculateNumber(-1.4, -2.6), -4);
-  });
-
-  it('should return 2 when inputting -1.4 and 2.6', () => {
-    assert.strictEqual(calculateNumber(-1.4, 2.6), 2);
-  });
-
-  it('should return -2 when inputting 1.4 and -2.6', () => {
-    assert.strictEqual(calculateNumber(1.4, -2.6), -2);
+  it('should return -4 when inputting -1.7 and -2', () => {
+    assert.strictEqual(calculateNumber(-1.7, -2), -4);
   });
 });
