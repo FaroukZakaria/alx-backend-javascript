a336b901290387f4d3eb3a0b17c7c5508a4552b1
FaroukZakaria <GamesWithoutLose97@gmail.com>
2024-02-29 14:52:27 +0000

    More test and edge cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,12 +1,24 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const calculateNumber = require('./0-calcul.js');
 
-describe('calculateNumber', function() {
-    it('should return the sum of two rounded numbers', function() {
-        assert.strictEqual(calculateNumber(1.4, 2.6), 4);
-        assert.strictEqual(calculateNumber(0, 0), 0);
-        assert.strictEqual(calculateNumber(-1.4, -2.6), -4);
-        assert.strictEqual(calculateNumber(-1.4, 2.6), 2);
-        assert.strictEqual(calculateNumber(1.4, -2.6), -2);
-    });
+describe('calculateNumber', () => {
+  it('should return 4 when inputting 1.4 and 2.6', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.6), 4);
+  });
+  
+  it('should return 0 when inputting 0 and 0', () => {
+    assert.strictEqual(calculateNumber(0, 0), 0);
+  });
+
+  it('should return -4 when inputting -1.4 and -2.6', () => {
+  assert.strictEqual(calculateNumber(-1.4, -2.6), -4);
+  });
+
+  it('should return 2 when inputting -1.4 and 2.6', () => {
+    assert.strictEqual(calculateNumber(-1.4, 2.6), 2);
+  });
+
+  it('should return -2 when inputting 1.4 and -2.6', () => {
+    assert.strictEqual(calculateNumber(1.4, -2.6), -2);
+  });
 });
